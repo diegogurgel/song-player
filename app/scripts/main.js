dd7da4fb29e2a5aefe6723e3f9062ba308e21e74
diego <diego@diegogurgel.com>
2014-04-18 02:56:15 -0300

    btn play pause

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,60 @@
-console.log('\'Allo \'Allo!');
+(function () {
+	player  = document.getElementsByTagName('audio')[0];
+	$(".btn-play").click(function(){
+		if(player.paused){
+			player.play();
+			$(".btn-play").addClass("pause");
+		}else{
+			player.pause();
+			$(".btn-play").removeClass("pause");
+		}
+	});
+
+
+	player.next = next;
+	player.indexSong = 0
+	player.onended = next;
+
+
+		songs = {
+		titles:["let_her","all_off_me"],
+
+	};
+
+	function next () {
+		player.indexSong++;
+		if(player.indexSong===songs.titles.length){
+			player.indexSong=0;
+		}
+		player.src="songs/"+songs.titles[player.indexSong]+".mp3";
+		player.play();
+	}
+	function prev (pos){
+		player.indexSong--;
+		if(player.indexSong===0){
+			player.indexSong=songs.titles.length-1;	
+		}		
+		player.src="songs/"+songs.titles[player.indexSong]+".mp3";
+		player.play();
+	}
+
+
+})();
+
+
+	function preLoadNextSong(){
+		$.ajax({
+			url: 'songs/let_her.mp3',
+			type: 'GET',
+		})
+		.done(function() {
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	}
